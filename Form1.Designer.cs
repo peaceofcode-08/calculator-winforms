9ff80bd2f1d0cdbbe66c9ebf53b764aee6087f99
peaceofcode-08 <amanfatima0208@gmail.com>
2025-07-20 16:43:41 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,368 @@
+﻿namespace MyCaclulator
+{
+    partial class Calc
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button2 = new Button();
+            button1 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button15 = new Button();
+            button16 = new Button();
+            button18 = new Button();
+            button19 = new Button();
+            button20 = new Button();
+            textBox_Result = new TextBox();
+            labelCurrentOperation = new Label();
+            SuspendLayout();
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Salmon;
+            button2.Font = new Font("Arial Rounded MT Bold", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.ForeColor = Color.Maroon;
+            button2.Location = new Point(264, 163);
+            button2.Name = "button2";
+            button2.Size = new Size(58, 45);
+            button2.TabIndex = 2;
+            button2.Text = "/";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += operator_click;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Salmon;
+            button1.Font = new Font("Arial Rounded MT Bold", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.Maroon;
+            button1.Location = new Point(333, 163);
+            button1.Name = "button1";
+            button1.Size = new Size(67, 45);
+            button1.TabIndex = 3;
+            button1.Text = "CE";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Salmon;
+            button3.Font = new Font("Arial Rounded MT Bold", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button3.ForeColor = Color.Maroon;
+            button3.Location = new Point(185, 163);
+            button3.Name = "button3";
+            button3.Size = new Size(58, 45);
+            button3.TabIndex = 4;
+            button3.Text = "9";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button_click;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.Salmon;
+            button4.Font = new Font("Arial Rounded MT Bold", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button4.ForeColor = Color.Maroon;
+            button4.Location = new Point(105, 163);
+            button4.Name = "button4";
+            button4.Size = new Size(58, 45);
+            button4.TabIndex = 5;
+            button4.Text = "8";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button_click;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.Salmon;
+            button5.Font = new Font("Arial Rounded MT Bold", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button5.ForeColor = Color.Maroon;
+            button5.Location = new Point(27, 163);
+            button5.Name = "button5";
+            button5.Size = new Size(58, 45);
+            button5.TabIndex = 6;
+            button5.Text = "7";
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += button_click;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.Salmon;
+            button6.Font = new Font("Arial Rounded MT Bold", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button6.ForeColor = Color.Maroon;
+            button6.Location = new Point(27, 224);
+            button6.Name = "button6";
+            button6.Size = new Size(58, 45);
+            button6.TabIndex = 11;
+            button6.Text = "4";
+            button6.UseVisualStyleBackColor = false;
+            button6.Click += button_click;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.Salmon;
+            button7.Font = new Font("Arial Rounded MT Bold", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button7.ForeColor = Color.Maroon;
+            button7.Location = new Point(105, 224);
+            button7.Name = "button7";
+            button7.Size = new Size(58, 45);
+            button7.TabIndex = 10;
+            button7.Text = "5";
+            button7.UseVisualStyleBackColor = false;
+            button7.Click += button_click;
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.Salmon;
+            button8.Font = new Font("Arial Rounded MT Bold", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button8.ForeColor = Color.Maroon;
+            button8.Location = new Point(185, 224);
+            button8.Name = "button8";
+            button8.Size = new Size(58, 45);
+            button8.TabIndex = 9;
+            button8.Text = "6";
+            button8.UseVisualStyleBackColor = false;
+            button8.Click += button_click;
+            // 
+            // button9
+            // 
+            button9.BackColor = Color.Salmon;
+            button9.Font = new Font("Arial Rounded MT Bold", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button9.ForeColor = Color.Maroon;
+            button9.Location = new Point(333, 224);
+            button9.Name = "button9";
+            button9.Size = new Size(67, 45);
+            button9.TabIndex = 8;
+            button9.Text = "C";
+            button9.UseVisualStyleBackColor = false;
+            button9.Click += button9_Click;
+            // 
+            // button10
+            // 
+            button10.BackColor = Color.Salmon;
+            button10.Font = new Font("Arial Rounded MT Bold", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button10.ForeColor = Color.Maroon;
+            button10.Location = new Point(264, 224);
+            button10.Name = "button10";
+            button10.Size = new Size(58, 45);
+            button10.TabIndex = 7;
+            button10.Text = "*";
+            button10.UseVisualStyleBackColor = false;
+            button10.Click += operator_click;
+            // 
+            // button11
+            // 
+            button11.BackColor = Color.Salmon;
+            button11.Font = new Font("Arial Rounded MT Bold", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button11.ForeColor = Color.Maroon;
+            button11.Location = new Point(27, 285);
+            button11.Name = "button11";
+            button11.Size = new Size(58, 45);
+            button11.TabIndex = 16;
+            button11.Text = "1";
+            button11.UseVisualStyleBackColor = false;
+            button11.Click += button_click;
+            // 
+            // button12
+            // 
+            button12.BackColor = Color.Salmon;
+            button12.Font = new Font("Arial Rounded MT Bold", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button12.ForeColor = Color.Maroon;
+            button12.Location = new Point(105, 285);
+            button12.Name = "button12";
+            button12.Size = new Size(58, 45);
+            button12.TabIndex = 15;
+            button12.Text = "2";
+            button12.UseVisualStyleBackColor = false;
+            button12.Click += button_click;
+            // 
+            // button13
+            // 
+            button13.BackColor = Color.Salmon;
+            button13.Font = new Font("Arial Rounded MT Bold", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button13.ForeColor = Color.Maroon;
+            button13.Location = new Point(185, 285);
+            button13.Name = "button13";
+            button13.Size = new Size(58, 45);
+            button13.TabIndex = 14;
+            button13.Text = "3";
+            button13.UseVisualStyleBackColor = false;
+            button13.Click += button_click;
+            // 
+            // button15
+            // 
+            button15.BackColor = Color.Salmon;
+            button15.Font = new Font("Arial Rounded MT Bold", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button15.ForeColor = Color.Maroon;
+            button15.Location = new Point(264, 285);
+            button15.Name = "button15";
+            button15.Size = new Size(58, 45);
+            button15.TabIndex = 12;
+            button15.Text = "-";
+            button15.UseVisualStyleBackColor = false;
+            button15.Click += operator_click;
+            // 
+            // button16
+            // 
+            button16.BackColor = Color.Salmon;
+            button16.Font = new Font("Arial Rounded MT Bold", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button16.ForeColor = Color.Maroon;
+            button16.Location = new Point(27, 347);
+            button16.Name = "button16";
+            button16.Size = new Size(136, 45);
+            button16.TabIndex = 21;
+            button16.Text = "0";
+            button16.UseVisualStyleBackColor = false;
+            button16.Click += button_click;
+            // 
+            // button18
+            // 
+            button18.BackColor = Color.Salmon;
+            button18.Font = new Font("Arial Rounded MT Bold", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button18.ForeColor = Color.Maroon;
+            button18.Location = new Point(185, 347);
+            button18.Name = "button18";
+            button18.Size = new Size(58, 45);
+            button18.TabIndex = 19;
+            button18.Text = ".";
+            button18.UseVisualStyleBackColor = false;
+            button18.Click += button_click;
+            // 
+            // button19
+            // 
+            button19.BackColor = Color.Salmon;
+            button19.Font = new Font("Arial Rounded MT Bold", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button19.ForeColor = Color.Maroon;
+            button19.Location = new Point(333, 285);
+            button19.Name = "button19";
+            button19.Size = new Size(67, 107);
+            button19.TabIndex = 18;
+            button19.Text = "=";
+            button19.UseVisualStyleBackColor = false;
+            button19.Click += button19_Click;
+            // 
+            // button20
+            // 
+            button20.BackColor = Color.Salmon;
+            button20.Font = new Font("Arial Rounded MT Bold", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button20.ForeColor = Color.Maroon;
+            button20.Location = new Point(264, 347);
+            button20.Name = "button20";
+            button20.Size = new Size(58, 45);
+            button20.TabIndex = 17;
+            button20.Text = "+";
+            button20.UseVisualStyleBackColor = false;
+            button20.Click += operator_click;
+            // 
+            // textBox_Result
+            // 
+            textBox_Result.BackColor = SystemColors.ButtonFace;
+            textBox_Result.Font = new Font("Segoe UI", 14F, FontStyle.Bold);
+            textBox_Result.ForeColor = Color.Maroon;
+            textBox_Result.Location = new Point(27, 97);
+            textBox_Result.Name = "textBox_Result";
+            textBox_Result.Size = new Size(370, 45);
+            textBox_Result.TabIndex = 22;
+            textBox_Result.Text = "0";
+            textBox_Result.TextAlign = HorizontalAlignment.Right;
+            // 
+            // labelCurrentOperation
+            // 
+            labelCurrentOperation.AutoSize = true;
+            labelCurrentOperation.BackColor = Color.FromArgb(255, 192, 192);
+            labelCurrentOperation.Font = new Font("Segoe UI", 14F, FontStyle.Bold);
+            labelCurrentOperation.ForeColor = Color.Maroon;
+            labelCurrentOperation.Location = new Point(27, 56);
+            labelCurrentOperation.Name = "labelCurrentOperation";
+            labelCurrentOperation.Size = new Size(0, 38);
+            labelCurrentOperation.TabIndex = 23;
+            // 
+            // Calc
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.RosyBrown;
+            ClientSize = new Size(438, 426);
+            Controls.Add(labelCurrentOperation);
+            Controls.Add(textBox_Result);
+            Controls.Add(button16);
+            Controls.Add(button18);
+            Controls.Add(button19);
+            Controls.Add(button20);
+            Controls.Add(button11);
+            Controls.Add(button12);
+            Controls.Add(button13);
+            Controls.Add(button15);
+            Controls.Add(button6);
+            Controls.Add(button7);
+            Controls.Add(button8);
+            Controls.Add(button9);
+            Controls.Add(button10);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button1);
+            Controls.Add(button2);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            MaximizeBox = false;
+            Name = "Calc";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "calculator";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button2;
+        private Button button1;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button15;
+        private Button button16;
+        private Button button18;
+        private Button button19;
+        private Button button20;
+        private TextBox textBox_Result;
+        private Label labelCurrentOperation;
+    }
+}
